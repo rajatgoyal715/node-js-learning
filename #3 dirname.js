bb781210260c205ed322177098e938be3014ff0f
Rajat Goyal <rajatgoyal715@gmail.com>
2017-06-02 17:00:44 +0530

    Global objects

---
+++ #3 dirname.js
@@ -0,0 +1,8 @@
+// node can also determine the your directory by using '__dirname';
+
+// global objects
+// https://nodejs.org/docs/latest/api/globals.html
+
+
+console.log(__dirname);
+console.log(__filename);
