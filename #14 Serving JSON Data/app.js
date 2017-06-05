fcd024866a6339f4e05e9eaef8e4bdbc260236be
Rajat Goyal <rajatgoyal715@gmail.com>
2017-06-05 16:29:22 +0530

    Serving JSON Data

---
+++ #14 Serving JSON Data/app.js
@@ -0,0 +1,16 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+  console.log('Request was made: ' + req.url)
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  var myObj = {
+    name: 'Rajat',
+    job: 'Coder',
+    age: 29
+  };
+  res.end(JSON.stringify(myObj));
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('Listening now ');
