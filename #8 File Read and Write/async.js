5bc0671cdc6ded190f9c83620bac430df2b7fe05
Rajat Goyal <rajatgoyal715@gmail.com>
2017-06-05 11:38:11 +0530

    sample file name changed

--- #8 File Read and Write/async.js
+++ #8 File Read and Write/async.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 
-var file = fs.readFile('input.txt','utf8',function(err,data){
-    fs.writeFile('writeme.txt',data);
-    console.log(data);
+var file = fs.readFile('sample.txt','utf8',function(err,data){
+  fs.writeFile('output.txt',data);
+  console.log(data);
 });
 
-console.log('This is an instruction outside the sync file system.');
+console.log('This is an instruction outside the sync file system. This line gets printed earlier because of async.');
 
 // to delete the file
-fs.unlink('writeme.txt');
+fs.unlink('output.txt');
