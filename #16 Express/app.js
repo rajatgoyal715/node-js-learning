3d1bbfcdb912d259a1de3e0c701a030052f07f37
Rajat Goyal <rajatgoyal715@gmail.com>
2017-06-05 17:37:31 +0530

    Template Engines

--- #16 Express/app.js
+++ #16 Express/app.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express();
 
+// set view engine - ejs
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('This is the homepage');
+  // send a message
+  // res.send('This is the homepage');
+  
+  // send a file
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function(req, res) {
@@ -12,7 +19,11 @@ app.get('/contact', function(req, res) {
 
 // dynamic route parameters
 app.get('/profile/:id', function(req, res) {
-  res.send('Requested to see profile with id of : ' + req.params.id);
+  // res.send('Requested to see profile with id of : ' + req.params.id);
+  
+  // dynamic rendering of HTML page
+  var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+  res.render('profile', {id: req.params.id, data: data});
 });
 
 app.listen(3000);
